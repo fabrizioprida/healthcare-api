e7d2540f55321e5570ec2c9910b91c845e51b886
Fabrizio Prida <fabrizio.prida@lightit.io>
2026-01-20 17:39:08 -0300

    feat: add delete clinic by id endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Container\Attributes\CurrentUser;
 use Illuminate\Support\Facades\Route;
+use Lightit\Clinics\App\Controllers\DeleteClinicController;
 use Lightit\Clinics\App\Controllers\GetClinicController;
 use Lightit\Clinics\App\Controllers\ListClinicController;
 use Lightit\Clinics\App\Controllers\StoreClinicController;
@@ -56,5 +57,6 @@ Route::prefix('clinics')
         Route::prefix('{clinic}')->group(static function (): void {
             Route::get('/', GetClinicController::class)->withTrashed();
             Route::put('/', UpdateClinicController::class);
+            Route::delete('/', DeleteClinicController::class);
         })->whereNumber('clinic');
     });
