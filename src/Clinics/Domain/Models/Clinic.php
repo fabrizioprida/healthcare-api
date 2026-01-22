b0bec994e144ac41312ca3aec9f6cd1a90eb5b53
Fabrizio Prida <fabrizio.prida@lightit.io>
2026-01-22 15:50:07 -0300

    style: update phpdocs

--- src/Clinics/Domain/Models/Clinic.php
+++ src/Clinics/Domain/Models/Clinic.php
@@ -7,11 +7,12 @@ namespace Lightit\Clinics\Domain\Models;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property int $id
- * @property string $name
- * @property string $address
+ * @property int                     $id
+ * @property string                  $name
+ * @property string                  $address
  * @property \Carbon\CarbonImmutable $created_at
  * @property \Carbon\CarbonImmutable $updated_at
+ *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Clinic newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Clinic newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Clinic query()
@@ -20,6 +21,7 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Clinic whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Clinic whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Clinic whereUpdatedAt($value)
+ *
  * @mixin \Eloquent
  */
 class Clinic extends Model
--- src/Users/Domain/Models/User.php
+++ src/Users/Domain/Models/User.php
@@ -12,18 +12,19 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 /**
- * @property int $id
- * @property string $name
- * @property mixed|null $email
+ * @property int                          $id
+ * @property string                       $name
+ * @property mixed|null                   $email
  * @property \Carbon\CarbonImmutable|null $email_verified_at
- * @property string $password
- * @property string|null $remember_token
- * @property \Carbon\CarbonImmutable $created_at
- * @property \Carbon\CarbonImmutable $updated_at
+ * @property string                       $password
+ * @property string|null                  $remember_token
+ * @property \Carbon\CarbonImmutable      $created_at
+ * @property \Carbon\CarbonImmutable      $updated_at
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
  * @property-read int|null $notifications_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
  * @property-read int|null $tokens_count
+ *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
@@ -35,6 +36,7 @@ use Laravel\Sanctum\HasApiTokens;
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
+ *
  * @mixin \Eloquent
  */
 class User extends Authenticatable
